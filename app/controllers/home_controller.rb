b7402e46baa235ee5410a9a61e60b3ab62509941
Thiago-Cardoso <trovador182@gmail.com>
2018-06-24 00:30:26 -0300

    challenge result alcohol or gasoline

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def challenge
+    @alcohol = params[:alcohol]
+    @gasoline = params[:gasoline]
+    resultado = Float(@alcohol) / Float(@gasoline)
+    @resultado = resultado.round(2)
+    if @resultado > 0.70
+      mensagem = "G"
+      render json: {"value":  mensagem }
+    else 
+      mensagem = "A"
+        render json: {"value":  mensagem }
+    end
+  end
 end
